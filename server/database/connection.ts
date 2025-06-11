abe352503b7ba89abcec77a7a80998ce11e6e3d5
unknown <ramovic225@gmail.com>
2025-06-11 19:13:40 +0200

    gitignore saved

---
+++ server/database/connection.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Connection failed:", error);
+    throw error;
+  }
+};
+
+export default connectDB;
