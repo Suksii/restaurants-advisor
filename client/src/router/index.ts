72450785ff75a2663959dfd905b73da55f5ed58a
unknown <ramovic225@gmail.com>
2025-06-12 19:17:03 +0200

    LoginView created

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -13,6 +13,12 @@ const router = createRouter({
       name: 'register',
       component: () => import('../views/RegisterView.vue'),
     },
+
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
   ],
 })
 
--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import LockIcon from '@/icons/LockIcon.vue'
-import registerBg from '../assets/restaurant-bg.jpeg'
+import loginBg from '../assets/loginbg.jpg'
 import UserIcon from '@/icons/UserIcon.vue'
-import MailIcon from '@/icons/MailIcon.vue'
 </script>
 
 <template>
   <div
-    :style="{ backgroundImage: `url('${registerBg}')` }"
+    :style="{ backgroundImage: `url('${loginBg}')` }"
     class="w-full min-h-screen bg-cover bg-no-repeat flex items-center justify-center"
   >
     <div class="w-[400px] bg-black/60 backdrop-blur-md rounded-xl p-8 shadow-lg">
       <h2 class="text-white uppercase text-2xl text-center tracking-wider font-semibold mb-6">
-        Sign up here
+        Sign in here
       </h2>
       <form class="flex flex-col gap-6">
         <div class="flex flex-col gap-1">
@@ -24,17 +23,6 @@ import MailIcon from '@/icons/MailIcon.vue'
             <input type="text" placeholder="Enter your username" class="register-input" />
           </div>
         </div>
-
-        <div class="flex flex-col gap-1">
-          <label class="text-white text-sm">Email</label>
-          <div class="relative">
-            <MailIcon
-              class="absolute left-3 top-1/2 transform -translate-y-1/2 text-white w-5 h-5"
-            />
-            <input type="email" placeholder="Enter your email" class="register-input" />
-          </div>
-        </div>
-
         <div class="flex flex-col gap-1">
           <label class="text-white text-sm">Password</label>
           <div class="relative">
@@ -44,14 +32,14 @@ import MailIcon from '@/icons/MailIcon.vue'
             <input type="password" placeholder="Enter your password" class="register-input" />
           </div>
         </div>
-        <button type="submit" class="register-button">Register</button>
+        <button type="submit" class="register-button">Login</button>
         <p class="text-center text-white/80 text-sm mt-4">
-          Already have an account?
+          Don't have an account?
           <RouterLink
-            to="/login"
+            to="/register"
             class="text-yellow-400 hover:underline hover:text-yellow-300 transition"
           >
-            Sign in
+            Sign up
           </RouterLink>
         </p>
       </form>
