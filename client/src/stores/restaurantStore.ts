fed902928c548cfa413852427ead0d396e38469a
unknown <ramovic225@gmail.com>
2025-06-18 16:25:45 +0200

    useRestaurantStore created (addRestaurant action)

--- client/src/stores/restaurantStore.ts
+++ client/src/stores/restaurantStore.ts
@@ -1,1 +1,16 @@
+import type { RestaurantPayload } from './../utils/types'
+import { addRestaurantService } from '@/services/restaurant'
+import { defineStore } from 'pinia'
 
+export const useRestaurantStore = defineStore('restaurant', {
+  state: () => ({
+    restaurantData: null as RestaurantPayload | null,
+  }),
+  actions: {
+    async addRestaurant(payload: RestaurantPayload) {
+      const response = await addRestaurantService(payload)
+      this.restaurantData = response?.data
+      return response
+    },
+  },
+})
