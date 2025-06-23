2b384181506a059fcc3830439ad6236e07472471
unknown <ramovic225@gmail.com>
2025-06-23 18:38:48 +0200

    image transition added

--- client/src/views/RestaurantView.vue
+++ client/src/views/RestaurantView.vue
@@ -25,18 +25,30 @@ onMounted(() => getRestaurant())
   <div class="w-full md:max-w-7xl mx-auto py-12">
     <div class="flex">
       <div class="flex flex-col gap-2 flex-1">
-        <img
-          :src="restaurant?.images[selectedImageIndex]?.secure_url"
-          class="w-full h-[50vh] object-cover rounded-md"
-        />
+        <Transition
+          enter-active-class="transition-opacity duration-200"
+          leave-active-class="transition-opacity duration-200"
+          enter-from-class="opacity-0"
+          enter-to-class="opacity-100"
+          mode="out-in"
+        >
+          <img
+            :src="restaurant?.images[selectedImageIndex]?.secure_url"
+            :key="restaurant?.images[selectedImageIndex]?.secure_url"
+            class="w-full h-[50vh] object-cover rounded-md"
+          />
+        </Transition>
         <div class="flex gap-4">
           <div
             v-for="(image, index) of restaurant?.images"
             :key="image.secure_url"
             @click="selectedImageIndex = index"
-            class="flex-1 cursor-pointer"
+            class="flex-1 cursor-pointer overflow-hidden rounded-md"
           >
-            <img :src="image.secure_url" class="object-cover h-48 w-full rounded-md" />
+            <img
+              :src="image.secure_url"
+              class="object-cover h-48 w-full hover:scale-105 transition-transform ease-in-out duration-200"
+            />
           </div>
         </div>
       </div>
